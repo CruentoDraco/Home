7520a89d96726667cc0b4c6c382f9b06ee4d54d8
CruentoDraco <re-deubzer@t-online.de>
2021-09-18 19:52:27 +0200

    added releases & mobile changes

--- scripts/script.js
+++ scripts/script.js
@@ -12,6 +12,12 @@ function setPage(){
         case "branding":
             $('#view').load('includes/branding.html');
         break;
+        case "releases":
+            $('#view').load('includes/releases.html');
+        break;
+        case "setup":
+            $('#view').load('includes/setup.html');
+        break;
         default:
             $('#view').load('includes/home.html');
         break;
